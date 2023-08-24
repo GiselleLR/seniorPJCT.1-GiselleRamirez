464467d3479af0db674a130a9705633d51ca7638
GiselleLR Ramirez <giselleramirez0929@gmail.com>
2023-08-24 17:45:14 +0000

    Space caulculator pjct.

--- GiselleLindsay.py
+++ GiselleLindsay.py
@@ -1,1 +1,9 @@
-print("Hello period three!")
+print("Wellcome to The Space Travel Calculator!")
+
+distance =  int(input("Enter a distance in light years!"))
+
+speed = int(input("Please enter a spacecraft speed in fraction of the speed of light!"))
+
+time = (distance/speed)
+
+print("It will take", time ,"hours to reach your desired location!")
