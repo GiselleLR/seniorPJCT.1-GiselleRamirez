8f87710280def70040016a4ca4c00c485d33d3b8
GiselleLR Ramirez <giselleramirez0929@gmail.com>
2023-08-31 18:06:05 +0000

    hi

---
+++ PairProject-pjct1-0.py
@@ -0,0 +1,12 @@
+print("You are off on your space mission! You've just taken off and left the Earth's atmosphere. Your team is ready for an adventure of a lifetime! The goal of this mission is to try to find life in the universe. As the captain - you now have an important choice. Where do you go?")
+userInput = input("Enter A or B. A) Head towards Mars B) Head into empty space")
+
+if userInput == 'A':
+# Student 1 finishes this code
+
+elif userInput == 'B':
+     print("Uh oh! You've reached the end of the road! You and your crew drifted off into endless space, and were never seen again.")
+     print("hi")
+else: 
+     print("You entered something wrong - refresh and try again!")
+ 
